46b79a3b5937609c85612cebc069615ac19dc1cb
ganrad <gradhakr@Ganeshs-MacBook-Air.local>
2016-01-21 12:39:05 -0800

    01-21-2016_2
    Using microtime() function to compute time start, end.

--- index.php
+++ index.php
@@ -3,13 +3,22 @@
 <body>
 
 <?php 
-$date_st = new DateTime();
-$y = rand(5000,30000);
+/**
+ * Calculate micro seconds
+ */
+function microtime_float() {
+	list($usec,$sec) = explode(" ", microtime());
+	return ((float)$usec + (float)$sec);
+}
+
+$time_st = microtime_float();
+$y = rand(25000,90000);
 for ($x=0 ; $x <= $y; $x++);
-$date_end = new DateTime();
+$time_end = microtime_float();
+
 # sleep(2); # Sleep for 2 seconds before returning
-$e_time = $date_end - $date_st;
-echo "<h2>The number is: $y, elapsed time: $e_time (msec)</h2>";
+$e_time = $time_end - $time_st;
+echo "<h2>The number is: $y, elapsed time: $e_time (Seconds)</h2>";
 ?>   
 
 </body>
