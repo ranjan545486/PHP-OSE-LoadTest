9b15b3b1fadbc2ddc18c9595e922559fa758b1a8
ganrad <gradhakr@Ganeshs-MacBook-Air.local>
2016-01-21 12:20:49 -0800

    01-21-2016_1
    Increased the loop range.

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 
 <?php 
 $date_st = new DateTime();
-$y = rand(400,750);
+$y = rand(5000,30000);
 for ($x=0 ; $x <= $y; $x++);
 $date_end = new DateTime();
-sleep(2); # Sleep for 2 seconds before returning
+# sleep(2); # Sleep for 2 seconds before returning
 $e_time = $date_end - $date_st;
 echo "<h2>The number is: $y, elapsed time: $e_time (msec)</h2>";
 ?>   
