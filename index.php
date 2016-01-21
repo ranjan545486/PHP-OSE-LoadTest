08ef44827a71f438066c11eff15a60b39bb5d28b
ganrad <gradhakr@Ganeshs-MacBook-Air.local>
2016-01-21 12:50:30 -0800

    01-21-2016_4
    Adding reverse loops.

--- index.php
+++ index.php
@@ -12,21 +12,28 @@ function microtime_float() {
 }
 
 $time_st = microtime_float();
-$y = rand(25000,90000);
-for ($x=0 ; $x <= $y; $x++);
-$y = rand(50000,90000);
-for ($x=0 ; $x <= $y; $x++);
-$y = rand(75000,90000);
-for ($x=0 ; $x <= $y; $x++);
-$y = rand(85000,90000);
-for ($x=0 ; $x <= $y; $x++);
+
 $y = rand(95000,100000);
-for ($x=0 ; $x <= $y; $x++);
+for ($x=0 ; $x <= $y; $x++)
+	for (; $y >= 0; $y--);
+$y = rand(95000,100000);
+for ($x=0 ; $x <= $y; $x++)
+	for (; $y >= 0; $y--);
+$y = rand(95000,100000);
+for ($x=0 ; $x <= $y; $x++)
+	for (; $y >= 0; $y--);
+$y = rand(95000,100000);
+for ($x=0 ; $x <= $y; $x++)
+	for (; $y >= 0; $y--);
+$y = rand(95000,100000);
+for ($x=0 ; $x <= $y; $x++)
+	for (; $y >= 0; $y--);
+
 $time_end = microtime_float();
 
 # sleep(2); # Sleep for 2 seconds before returning
 $e_time = $time_end - $time_st;
-echo "<h2>The number is: $y, elapsed time: $e_time (Seconds)</h2>";
+echo "<h2>Elapsed time: $e_time (Seconds)</h2>";
 ?>   
 
 </body>
